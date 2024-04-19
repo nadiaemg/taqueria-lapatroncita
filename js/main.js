fcce482caca3d57ed1823a30e8c4d01ceccdeca8
nadiaemg <nademarg@gmail.com>
2024-04-19 04:51:23 -0600

    Se agregó funcionalidad de formulario de suscripción del footer.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const header = document.querySelector("header");
 const footer = document.querySelector("footer");
-let url = window.location.href;
-let regexEmail = new RegExp("^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$");
+const url = window.location.href;
+const regexEmail = new RegExp("^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$");
+const scriptURL = "https://script.google.com/macros/s/AKfycbx_CfROdeKRBfQ-IHgoddFnRF6Elol41OAaPG--FGCoLInZbep1lZZ4PAGFHPFpspix/exec";
 
 function addNavbar() {
     header.insertAdjacentHTML("afterbegin", `
@@ -25,6 +26,7 @@ function addNavbar() {
     const navbar = document.getElementById("navbar")
     const navElementos = document.getElementById("navElementos");
 
+    /*Navegación - Página actual*/
     if (url.includes("index.html")) {
         document.getElementById("inicio").setAttribute("aria-current", "page");
     } else if (url.includes("menu.html")) {
@@ -35,6 +37,7 @@ function addNavbar() {
         document.getElementById("contacto").setAttribute("aria-current", "page");
     }
 
+    /*Menú hamburguesa*/
     botonAbrir.addEventListener("click", function () {
         navElementos.classList.add("nav-visible");
         //console.log("click");
@@ -51,6 +54,7 @@ function addNavbar() {
         }
     });
 
+    /*Navbar dinámico al hacer scroll*/
     window.addEventListener("scroll", function () {
         if (window.scrollY > 0) {
             navbar.classList.add("scrolled");
@@ -80,17 +84,22 @@ function addFooter() {
                 <form name="submit-to-google-sheet">
                     <div class="mb-3">
                         <label for="correoSuscripcion" class="form-label widgets">Únete a la comunidad de suscriptores para recibir noticias y ofertas exclusivas.</label>
+                        <p class="widgets" id="alertaSuscripcion" style="font-weight: 700;"></p>
+                        <p class="widgets" id="alertaCorreo" style="font-weight: 700;"></p>
                         <div class="email-contenedor">
-                            <input type="email" class="form-control" id="correoSuscripcion" placeholder="Correo electrónico" name="email"
+                            <input type="email" class="form-control" id="correoSuscripcion" placeholder="Correo electrónico" name="email" autocomplete="email"
                                 required />
                             <button class="boton-amarillo" type="submit" id="btnSuscripcion">
                                 Suscribirse
                             </button>
                         </div>
                     </div>
+                    <div>
+                        <p class="widgets" id="alertaPoliticas" style="font-weight: 700;"></p>
+                    </div>
                     <div class="checkbox-politicas">
-                        <input type="checkbox" class="form-check-input" id="politicasSuscripcion" required />
-                        <label for="politicasSuscripcion" class="form-check-label widgets">He leído y acepto la
+                        <input type="checkbox" class="form-check-input" id="checkboxPoliticas" required />
+                        <label for="checkboxPoliticas" class="form-check-label widgets">He leído y acepto la
                         <a href="politica.html" target="_blank">política de privacidad</a>.</label>
                     </div>
                 </form>
@@ -117,6 +126,76 @@ function addFooter() {
     </footer>
     `;
 
+    /*Formulario de suscripción*/
+    const alertaSuscripcion = document.getElementById("alertaSuscripcion");
+    const alertaCorreo = document.getElementById("alertaCorreo");
+    const alertaPoliticas = document.getElementById("alertaPoliticas");
+    const btnSuscripcion = document.getElementById("btnSuscripcion");
+    const inputCorreo = document.getElementById("correoSuscripcion");
+    const checkboxPoliticas = document.getElementById("checkboxPoliticas");
+    const form = document.forms['submit-to-google-sheet'];
+
+    inputCorreo.addEventListener("change", function(){
+        alertaCorreo.innerText= "";
+    })
+
+    checkboxPoliticas.addEventListener("click", function(){
+        alertaPoliticas.innerText = "";
+    })
+
+    btnSuscripcion.addEventListener("click", function (event) {
+        event.preventDefault();
+        console.log("click");
+        alertaSuscripcion.innerText= "";
+        alertaCorreo.innerText= "";
+        alertaPoliticas.innerText= "";
+
+        if (!checkboxPoliticas.checked) {
+            alertaPoliticas.style.color = "var(--rusty-red)";
+            alertaPoliticas.innerText= "Por favor, marque la casilla para continuar.";
+            setTimeout(() => {
+                alertaPoliticas.innerText = "";
+            }, 30000);
+            checkboxPoliticas.focus();
+        }
+        if (!regexEmail.test(inputCorreo.value)) {
+            alertaCorreo.style.color = "var(--rusty-red)";
+            alertaCorreo.innerText= "Por favor, ingrese un correo válido.";
+            setTimeout(() => {
+                alertaCorreo.innerText = "";
+            }, 30000);
+            inputCorreo.focus();
+        }
+        if (checkboxPoliticas.checked && regexEmail.test(inputCorreo.value)) {
+            alertaSuscripcion.style.color = "var(--avocado)";
+            alertaSuscripcion.innerText= "Enviando datos...";
+
+            fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error("Error");
+                    }else{
+                    console.log("Todo bien: ", response);
+                    inputCorreo.value = "";
+                    checkboxPoliticas.checked = false;
+                    alertaSuscripcion.style.color = "var(--avocado)";
+                    alertaSuscripcion.innerText= "¡Suscripción exitosa!";
+                    setTimeout(() => {
+                        alertaSuscripcion.innerText = "";
+                    }, 30000);
+                    }
+                })
+                .catch(error => {
+                    console.error("Error: ", error.message)
+                    alertaSuscripcion.style.color = "var(--rusty-red)";
+                    alertaSuscripcion.innerText= "Por el momento el servicio no está disponible. Inténtelo más tarde";
+                    setTimeout(() => {
+                        alertaSuscripcion.innerText = "";
+                    }, 30000);
+                });
+        }
+    });
+
 }
 
 window.addEventListener("load", function (event) {
@@ -124,4 +203,5 @@ window.addEventListener("load", function (event) {
     //window.scrollTo(0,0);
     addNavbar();
     addFooter();
+
 });
